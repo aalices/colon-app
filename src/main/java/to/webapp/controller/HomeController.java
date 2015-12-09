94da12fb0feb0f6625a18b4a0481825630b72647
aalices <aa.warchal@gmail.com>
2015-12-09 10:17:42 +0100

    set up communication between Spring and Backbone

--- src/main/java/to/webapp/controller/HomeController.java
+++ src/main/java/to/webapp/controller/HomeController.java
@@ -4,15 +4,21 @@ import java.io.IOException;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
+import to.webapp.User;
 
 @Controller
+@RequestMapping("/api")
 public class HomeController {
 
-	@RequestMapping(value="/")
-	public ModelAndView test(HttpServletResponse response) throws IOException{
-		return new ModelAndView("home");
+	@RequestMapping(method = RequestMethod.GET, produces = "application/json") @ResponseBody
+	public User findAll() {
+		return new User("aaa", "bbb");
 	}
 }
